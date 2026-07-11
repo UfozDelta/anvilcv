cdb96baec35386a7486fe26ddca76195eb7e5e9c
UfozDelta <chizblitz@gmail.com>
2026-07-11 15:48:29 -0400

    bug(merge) Looks like merge didnt go correclty has been fixed

--- src/main/resources/db/migration/V13__pipeline_duration.sql
+++ src/main/resources/db/migration/V13__pipeline_duration.sql
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 ALTER TABLE application
     ADD COLUMN pipeline_duration_ms BIGINT;
-=======
-ALTER TABLE application ADD COLUMN pipeline_duration_ms BIGINT;
->>>>>>> application_timing
